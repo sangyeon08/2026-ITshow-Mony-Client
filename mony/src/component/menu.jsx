c374a60ed5ef51b822c3fc92a2d1b97abea28fef
slee <sanoni0115@gmail.com>
2026-06-16 22:45:51 +0900

    fix: Add padding & margin to menu bar

--- mony/src/component/menu.jsx
+++ mony/src/component/menu.jsx
@@ -103,10 +103,8 @@ export default function Menu() {
           이해하는 소비를 실천해 볼까요?
         </div>
         <div className="menu-bottom-context">
-          <span className="title">목적을 잃지 마라 !!</span>
-          돈을 아끼는 것이 미덕이지만,
-          돈을 아끼는 것이 목적이
-          되어서는 안 된다.
+          <span className="title">ㅤ목적을 잃지 마라 !!</span>
+          <span className="lion">ㅤ돈을 아끼는 것이 미덕이지만, ㅤ돈을 아끼는 것이 목적이  ㅤ ㅤ ㅤ되어서는 안 된다.</span>
         </div>
         <p className="menu-resetBtn" onClick={handleReset}>처음으로 가기</p>
       </div>
