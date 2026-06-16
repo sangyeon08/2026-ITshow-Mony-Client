2718c37a94f9a5feba8002adaceb654d1084f259
Yeseo0502 <5252yeseo@gmail.com>
2026-06-16 22:47:56 +0900

    수정

--- mony/src/component/menu.jsx
+++ mony/src/component/menu.jsx
@@ -104,7 +104,7 @@ export default function Menu() {
         </div>
         <div className="menu-bottom-context">
           <span className="title">ㅤ목적을 잃지 마라 !!</span>
-          <span className="lion">ㅤ돈을 아끼는 것이 미덕이지만, ㅤ돈을 아끼는 것이 목적이  ㅤ ㅤ ㅤ되어서는 안 된다.</span>
+          <span className="lion">ㅤ돈을 아끼는 것이 미덕이지만, ㅤ돈을 아끼는 것이 목적이  ㅤ ㅤ되어서는 안 된다.</span>
         </div>
         <p className="menu-resetBtn" onClick={handleReset}>처음으로 가기</p>
       </div>
