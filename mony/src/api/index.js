a24a04c2a9ca815043a8a2b659d83ae64dbc1bc0
Yeseo0502 <5252yeseo@gmail.com>
2026-06-17 20:57:05 +0900

    ca db연동

--- mony/src/api/index.js
+++ mony/src/api/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = '/api';
+const BASE_URL = 'http://localhost:3001/api';
 
 function getUserId() {
   const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
